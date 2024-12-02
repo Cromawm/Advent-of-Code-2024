87add9513219655ac7919aee8cb14a1566d9c6e0
Marco Wasmer Miguel <marco.wasmermiguel@gmail.com>
2024-12-02 13:47:16 +0000

    One liner added

--- 2/day2.py
+++ 2/day2.py
@@ -1,5 +1,5 @@
 def read_lines() -> list[str]:
-    with open("lists.txt") as f:
+    with open("lists.txt") as f: 
         return f.readlines()
 
 def parse_record(record: str) -> list[int]:
@@ -26,7 +26,7 @@ def is_safe (l: list[int]) -> bool:
 def count_safe() -> int:
     return len(list(filter(is_safe, parse_records())))
 
-print(f"Day two part one: {count_safe()}") 
+# part two stuff
 
 def is_safe_dampened(l: list[int]) -> bool:
     return is_safe(l) or any(list(map(is_safe, [l[:i] + l[i+1:] for i in range(len(l))])))
@@ -34,10 +34,9 @@ def is_safe_dampened(l: list[int]) -> bool:
 def count_safe_dampened() -> int:
     return len(list(filter(is_safe_dampened, parse_records())))
 
+print(f"Day two part one: {count_safe()}") 
 print(f"Day two part two: {count_safe_dampened()}")
     
-
-
-
 # lmao we can write it as one line
 # with open("lists.txt") as f: print(f"Day two part one: {len(list(filter(lambda l: (all(x > y for (x, y) in list(zip(l, l[1:]))) or all(x < y for (x, y) in list(zip(l, l[1:])))) and all(4 > abs(x-y) for (x, y) in list(zip(l, l[1:]))), list(map(lambda r: list(map(int, r.split(" "))), f.readlines())))))}")
+# with open("lists.txt") as f: print(f"Day two part two: {len(list(filter(lambda l: ((all(x > y for (x, y) in list(zip(l, l[1:]))) or all(x < y for (x, y) in list(zip(l, l[1:])))) and all(4 > abs(x-y) for (x, y) in list(zip(l, l[1:])))) or any(list(map(lambda l: (all(x > y for (x, y) in list(zip(l, l[1:]))) or all(x < y for (x, y) in list(zip(l, l[1:])))) and all(4 > abs(x-y) for (x, y) in list(zip(l, l[1:]))), [l[:i] + l[i+1:] for i in range(len(l))]))), list(map(lambda record: list(map(int, record.split(" "))), f.readlines())))))}")
